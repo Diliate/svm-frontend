975db0270b0720629138daa6bcd92f29a998b9ba
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-11-19 22:30:09 +0530

    style: updated UI of product card

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -2,35 +2,36 @@ import Image from "next/image";
 import Link from "next/link";
 import React from "react";
 import { FaStar } from "react-icons/fa6";
+import { FaArrowRight } from "react-icons/fa6";
 
 const ProductCard = () => {
   return (
-    <div className="border-2 rounded-3xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] w-[275px] bg-[#F9F9EB] hover:scale-105 duration-200">
-      <Link href={"/product/1"}>
-        <Image src="/product.png" alt="product" height={290} width={290} />
-      </Link>
-      <div className="px-4 pb-5">
-        <p className="text-lg">Syrup</p>
-        <div className="flex justify-between my-2">
-          <h1 className="text-2xl font-semibold">Cuff-C</h1>
-          <div className="flex items-center gap-1">
-            {Array.from({ length: 5 }, (_, index) => (
-              <FaStar key={index} />
-            ))}
-          </div>
-        </div>
-        <p className="text-lg">Rs 1200</p>
+    <div className="border-2 rounded-3xl shadow-[0_3px_10px_rgb(0,0,0,0.2)] w-[275px] hover:scale-105 duration-200 p-4">
+      <div className="border-2 rounded-2xl bg-[#F9F9EB]">
+        <Image src="/product.png" alt="product" height={300} width={300} />
       </div>
-      <div className="">
-        <Link href={"/cart"}>
-          <button className="text-white bg-[#004A06] rounded-bl-3xl w-1/2 py-2 hover:opacity-85 duration-200">
-            Add to Cart
-          </button>
-        </Link>
-        <Link href={"/payment"}>
-          <button className="w-1/2 py-2 text-white duration-200 bg-black rounded-br-3xl hover:opacity-85">
-            Buy Now
-          </button>
+      <div className="flex justify-between my-2">
+        <h1 className="text-2xl font-semibold">Cuff-C Syrup</h1>
+        {/* <div className="flex items-center gap-1">
+                {Array.from({ length: 4 }, (_, index) => (
+                  <FaStar key={index} color="#FFB345" />
+                ))}
+              </div> */}
+      </div>
+
+      <p className="text-lg">
+        Its a syrup medicine which helps in reducing coungh.
+      </p>
+      <div className="flex items-center justify-between mt-5">
+        <p className="text-xl font-semibold">Rs 1200</p>
+        <Link
+          href={"/product/1"}
+          className="bg-[#166534] text-white rounded-full p-3 flex items-center justify-center group"
+        >
+          <FaArrowRight
+            className="duration-300 transform group-hover:translate-x-2"
+            size={24}
+          />
         </Link>
       </div>
     </div>
