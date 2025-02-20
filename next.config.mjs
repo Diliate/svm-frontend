915d980a9bbefb5806776128b1ba0b197f9121f9
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2025-02-20 12:09:24 +0530

    update: Next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,25 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      "localhost",
-      "svm-backend-iy0e.onrender.com",
-      "svmpharmaceutical.com",
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "svm-backend-iy0e.onrender.com",
+        pathname: "/uploads/",
+      },
     ],
+    minimumCacheTTL: 60,
+    dangerouslyAllowSVG: true,
+    contentSecurityPolicy: "default-src 'self'; script-src 'none'; sandbox;",
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/uploads/:path*",
+        destination: "https://svm-backend-iy0e.onrender.com/uploads/:path*",
+      },
+    ];
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
